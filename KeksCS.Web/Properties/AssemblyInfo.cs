d83c72eff24a962df520d221b2dec9b261fab7d9
Konstantin Lyakh <kostya.lyakh@gmail.com>
2017-02-25 20:58:03 +0200

    NuGet package.

--- KeksCS.Web/Properties/AssemblyInfo.cs
+++ KeksCS.Web/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("KeksCS.Web")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("ASP.NET web utilities.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Keks Custom Solutions")]
 [assembly: AssemblyProduct("KeksCS.Web")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
